05542d4fea194bf64fd11bd654f528f9b1ace99b
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-10-19 20:45:44 -0700

    Correcting Links

--- playground/data/menuLinksHeader.ts
+++ playground/data/menuLinksHeader.ts
@@ -3,17 +3,17 @@ import {MenuLinkType} from '../types';
 export const menuLinksHeader = [
     {
         label: "Author",
-        content: 'https://jaimin-pandya.netlify.app/',
+        content: 'https://jaimin-pandya.netlify.app',
         isOuterLink: true
     },
     {
         label: "NPM",
-        content: "https://www.npmjs.com/package/@jaiminmoslake7020/react-typing-text",
+        content: "https://www.npmjs.com/package/@jaiminmoslake7020/react-dark-light-theme-switcher",
         isOuterLink: true
     },
     {
         label: "Github",
-        content: "https://github.com/jaiminmoslake7020/react-typing-text",
+        content: "https://github.com/jaiminmoslake7020/react-dark-light-theme-switcher",
         isOuterLink: true
     }
 ] as MenuLinkType[];
